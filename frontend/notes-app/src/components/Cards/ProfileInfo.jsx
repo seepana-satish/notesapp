1cb02922517d5b4da45c61c09fdf7f7af9b58a72
seepana-satish <seepanasatish187@gmail.com>
2026-07-24 10:49:14 +0530

    update the prfofileINfo

--- frontend/notes-app/src/components/Cards/ProfileInfo.jsx
+++ frontend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -3,8 +3,9 @@ import { getInitials } from '../../utils/helper';
 
 const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
-    <div className='flex items-center gap-3'>
-      <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
+    <div className='flex items-center justify-between w-full gap-3'>
+      <div className='flex items-center gap-3'>
+        <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100'>
         {getInitials(userInfo?.fullName)}
       </div>
 
@@ -12,7 +13,7 @@ const ProfileInfo = ({ userInfo, onLogout }) => {
         <p className='text-sm font-medium'>
           {userInfo?.fullName}
         </p>
-
+      </div>
         <button
           className='text-sm text-slate-700 underline'
           onClick={onLogout}
